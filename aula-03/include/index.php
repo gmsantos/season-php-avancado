709fa74cda429e9811f8ade215fe24e7adff478d
gmsantos <gabriel.ms1@hotmail.com>
2016-05-04 21:34:34 -0300

    include e require

---
+++ aula-03/include/index.php
@@ -0,0 +1,9 @@
+<pre>
+Algum conteudo
+
+<?php include_once 'menu.html' ?>
+
+mais conteudo
+
+<?php include_once 'menu.html' ?>
+</pre>
---
+++ aula-03/include_require.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'https://raw.githubusercontent.com/gmsantos/season-php-avancado/master/aula-02/file_get_contents.php';
